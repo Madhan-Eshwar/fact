e2bc1a5c0c880938a2e08dc866ab2bbb618fea7c
Madhan-Eshwar <mailtomadhan95@gmail.com>
2016-08-13 14:27:30 +0530

    Update facto.c

--- facto.c
+++ facto.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+  int n,i,sum=0;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    sum=sum*i;
+  }
+  printf("%d",sum);
+  return 0;
+}
